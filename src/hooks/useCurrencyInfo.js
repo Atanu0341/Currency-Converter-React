418c375d97facfa2b961249e5c4ae053a43b07bc
Atanu Majumder <atanumajumder2004@gmail.com>
2024-09-23 21:40:44 +0530

    Update useCurrencyInfo.js

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency) {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-    )
+const useCurrencyInfo = (currency) => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+      fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
       .then((res) => res.json())
-      .then((res) => setData(res[currency]));
-    console.log(data);
-  }, [currency]);
-  console.log(data);
-  return data;
+      .then((res) => setData(res.rates))
+      .catch((error) => console.error("Error fetching currency data:", error));
+    }, [currency]);
+
+    return data;
 }
 
 export default useCurrencyInfo;
